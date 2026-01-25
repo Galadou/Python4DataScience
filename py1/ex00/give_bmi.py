f90a090a528d68f337d8c5899db2ae5941226742
Galadou <150854587+Galadou@users.noreply.github.com>
2026-01-25 23:10:29 +0100

    Add input validation to give_bmi function
    Enhanced the give_bmi function with type, value, and size checks for height and weight lists. The function now raises and prints assertion errors for invalid inputs and returns None on failure.

--- py1/ex00/give_bmi.py
+++ py1/ex00/give_bmi.py
@@ -5,6 +5,31 @@ import numpy as np
 # any = Si un bloc est true dedans, alors true.
 
 
+def give_bmi(height: list[int | float], weight: list[int | float]) \
+      -> list[int | float]:
+    try:
+        if not isinstance(height, list) \
+                or not all(isinstance(i, (int, float)) for i in height):
+            raise AssertionError("height should take only integer or float.")
+        if not isinstance(weight, list) \
+                or not all(isinstance(i, (int, float)) for i in weight):
+            raise AssertionError("weight should take only integer or float.")
+        if len(height) != len(weight):
+            raise AssertionError("weight and height have not the same size.")
+        np_height = np.array(height)
+        np_weight = np.array(weight)
+        if any(i <= 0 for i in np_height):
+            raise AssertionError("height cannot be negative or equal to 0.")
+        if any(i <= 0 for i in np_weight):
+            raise AssertionError("weight cannot be negative or equal to 0.")
+        bmi_result = np_weight / (np_height ** 2)
+        result = bmi_result.tolist()
+        return result
+    except AssertionError as error:
+        print(f"AssertionError: {error}")
+    except Exception as error:
+        print(f"{type(error)} : {error}")
+    return None
 
 
 def apply_limit(bmi: list[int | float], limit: int) -> list[bool]:
