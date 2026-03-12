92fe2c1a7a947738fb4c3f72f94d66db08365a69
Galadou <galaad.mersch@gmail.com>
2026-03-12 17:49:08 +0100

    ex05 cleaned

--- Python-1-Array/ex05/pimp_image.py
+++ Python-1-Array/ex05/pimp_image.py
@@ -15,6 +15,9 @@ def ft_invert(array: np.array) -> np.array:
 
 
 def ft_red(array: np.array) -> np.array:
+    """
+    Put a red filter to the image received.
+    """
     red_filter = array.copy()
     red_filter[:, :, 1] = 0
     red_filter[:, :, 2] = 0
@@ -25,6 +28,9 @@ def ft_red(array: np.array) -> np.array:
 
 
 def ft_green(array: np.array) -> np.array:
+    """
+    Put a green filter to the image received.
+    """
     green_filter = array.copy()
     green_filter[:, :, 0] = 0
     green_filter[:, :, 2] = 0
@@ -35,6 +41,9 @@ def ft_green(array: np.array) -> np.array:
 
 
 def ft_blue(array: np.array) -> np.array:
+    """
+    Put a blue filter to the image received.
+    """
     blue_filter = array.copy()
     blue_filter[:, :, 0] = 0
     blue_filter[:, :, 1] = 0
@@ -45,6 +54,9 @@ def ft_blue(array: np.array) -> np.array:
 
 
 def ft_grey(array: np.array) -> np.array:
+    """
+    Put a grey filter to the image received.
+    """
     np_grey = np.mean(array, axis=2, keepdims=True)
     np_grey = np.repeat(np_grey, 3, axis=2)
     np_grey = np_grey.astype(np.uint8)
