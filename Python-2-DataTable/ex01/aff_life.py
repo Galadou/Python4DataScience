aeebded84fc2226e3ffde1543a77efe57ab672e3
Galadou <galaad.mersch@gmail.com>
2026-03-13 18:33:37 +0100

    Python-2 ex00 & 01 done

---
+++ Python-2-DataTable/ex01/aff_life.py
@@ -0,0 +1,34 @@
+from load_csv import load
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+def main():
+    """
+    Load life_expectancy_years.csv, and create a plot with it.
+    """
+    try:
+        data = load("life_expectancy_years.csv")
+        if data is None:
+            return
+        france_data = pd.DataFrame
+        france_data = data.query("country == 'France'")
+        values = france_data.iloc[0, 1:]
+        years = france_data.columns[1:]
+        plt.plot(years, values)
+        plt.title("France Life expectancy Projections")
+        # xticks is used to change the legend of the x-axis.
+        # start : stop : step
+        plt.xticks(years[::40])
+        plt.xlabel("Year")
+        plt.ylabel("Life expectancy")
+        plt.show()
+
+    except KeyboardInterrupt as error:
+        print(f"{type(error)} : Interrupted.")
+    except Exception as error:
+        print(f"{type(error)} : {error}")
+
+
+if __name__ == "__main__":
+    main()
